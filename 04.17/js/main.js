06a9842197b1b6009c98f5b33297105c7f8c8f9c
SARAH <sarah.h.halevi@gmail.com>
2023-04-19 17:12:25 +0300

    ex1

---
+++ 04.17/js/main.js
@@ -0,0 +1,43 @@
+//month=1, mysteryAmont=0, rentDue=0, gift=false;
+var month= 1;
+var mysteryAmont=0;
+var rentDue=0;
+var gift=false;
+
+var salary = prompt("Enter your salary:");
+var rent = promp("Enter your rent:");
+
+var balance = salary*2;
+if (balance>=rent){
+    balance-=rent;
+}
+else{
+    rentDue = rent;
+}
+month++;
+balance +=salary/2;
+
+if(balance >= rentDue){
+    balance -= rentDue;
+    rentDue = 0;
+}
+rent = rent +200;
+
+if (balance>=rent){
+    balance-=rent;
+}
+else{
+    rentDue += rent;
+}
+balance -=500;
+month++;
+mysteryAmont = balance;
+balance =0;
+
+if(mysteryAmont<0){
+    gift = true;
+    mysteryAmont = mysteryAmont *(-1);
+}
+
+alert(month, mysteryAmont, rentDue, gift);
+//output:month, mysteryAmont, rentDue, gift;
