a7ffed75af64a9af8b52e57b754c9e38507b287e
SARAH <sarah.h.halevi@gmail.com>
2023-05-03 00:12:22 +0300

    ex5

---
+++ 05.01/js/main.js
@@ -0,0 +1,9 @@
+
+var studen1 = prompt("Enter the first student:");
+var studen2 = prompt("Enter the second student:");
+var studen3 = prompt("Enter the third student:");
+var studen4 = prompt("Enter the fourth student:");
+var studen5 = prompt("Enter the fifth student:");
+var students = [studen1 + studen2 + studen3 + studen4 + studen5];
+students.sort().join(",", "  ");
+console.log(students);
