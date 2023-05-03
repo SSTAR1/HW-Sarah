285e485bc5c8f398168e392c986728d8f588d56f
SARAH <sarah.h.halevi@gmail.com>
2023-05-03 17:17:13 +0300

    ex6

--- 05.01/js/main.js
+++ 05.01/js/main.js
@@ -4,6 +4,6 @@ var studen2 = prompt("Enter the second student:");
 var studen3 = prompt("Enter the third student:");
 var studen4 = prompt("Enter the fourth student:");
 var studen5 = prompt("Enter the fifth student:");
-var students = [studen1 + studen2 + studen3 + studen4 + studen5];
-students.sort().join(",", "  ");
+var students = [studen1, studen2, studen3, studen4, studen5,];
+students.sort().join(", ");
 console.log(students);
