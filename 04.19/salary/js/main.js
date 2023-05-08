7bc7b721e0be41e43cb3801b6793beb210dbd403
SARAH <sarah.h.halevi@gmail.com>
2023-05-08 19:39:08 +0300

    ex11

--- 04.19/salary/js/main.js
+++ 04.19/salary/js/main.js
@@ -1,19 +1,24 @@
 var salary = +prompt("enter your salary:");
 var tax = salary * 0.1;
-var tax1 = 2500 * 0.1;
-var tax2 = 2500 * 0.2;
+var tax1 = 25000 * 0.1;
+var tax2 = 25000 * 0.2;
 var tax3 = 50000 * 0.3;
 //tax4 =  * 0.4;
+var diff = 0;
 if(0 > salary <= 2500 ){
     alert(tax)
 }
-else if(2500 < salary >= 50000){
-salary -= 25000;
-tax = salary * 0.2;//x
-alert(`You need to pay ${tax + tax1} tax`)
+else if(2500 < salary >= 50000){ 
+   diff = (salary - 25000) * 0.2;
+/*salary -= 25000;
+tax = salary * 0.2;//x*/
+alert(`You need to pay ${diff + tax1} tax`)
 }
 else if(50000 < salary >= 1000000){
     salary -= 50000;
     tax = salary * 0.3;
     alert(`You need to pay ${tax + tax1 + tax2} tax`)
 }
+
+
+//diff = salary - 25000;
