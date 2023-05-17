d1e414201c97bb1c1aa4892208a7e877b30e4e59
SARAH <sarah.h.halevi@gmail.com>
2023-05-17 07:30:10 +0300

    05.16

--- 05.01/js/main.js
+++ 05.01/js/main.js
@@ -1,9 +1,22 @@
 
-var studen1 = prompt("Enter the first student:");
+/*var studen1 = prompt("Enter the first student:");
 var studen2 = prompt("Enter the second student:");
 var studen3 = prompt("Enter the third student:");
 var studen4 = prompt("Enter the fourth student:");
-var studen5 = prompt("Enter the fifth student:");
+var studen5 = prompt("Enter the fifth student:");//לעשות פרומפט אחד ואז פוש. עם לולאה
 var students = [studen1, studen2, studen3, studen4, studen5,];
-students.sort().join(", ").toLocaleLowerCase();
+
+students.sort().join(", ").toLocaleLowerCase()//. להגדיר משתנה לגוינ וטו לוייר כייס 
 console.log(students);
+
+
+//כל עוד המתודה לו עושה שינוי לאריי- חייבים לתפוס אותו במשתנה
+*/
+var students = [];
+
+for (var i = 0; i < 5; i++) {
+  var student = prompt("Enter student " + (i + 1) + ":");
+  students.push(student);
+}
+var students2= students.sort().join(", ").toLocaleLowerCase()
+console.log(students2);
---
+++ 05.3/ex1/js/main.js
@@ -0,0 +1,16 @@
+var input = prompt("Enter text:").toLocaleLowerCase;
+input.split// אולי להוסיף משהו ואז לחלק אותו שיצא מערך שכל אות היא סטרינג בפני עצמה
+if (input != a | o | u | i |e ){
+    let consonants = ;
+    else {
+        let vowels = 
+    }
+}
+
+function vowels_consonants (x);
+
+
+
+//output array v + c
+function massage(y);
+alert(`Your string contains the following vowels|: ${vowels} and the following consonants: ${consonants} `);
