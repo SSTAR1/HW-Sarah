38e1222fad4d3d2726af53d47c0f5e885b601574
SARAH <sarah.h.halevi@gmail.com>
2023-05-20 23:55:59 +0300

    05.20 num1

--- 04.19/salary/js/main.js
+++ 04.19/salary/js/main.js
@@ -1,4 +1,40 @@
+//first try
+
+/*
 var salary = +prompt("enter your salary:");
+var tax = salary * 0.1;
+var tax1 = 2500 * 0.1;
+var tax2 = 2500 * 0.2;
+var tax1 = 25000 * 0.1;
+var tax2 = 25000 * 0.2;
+var tax3 = 50000 * 0.3;
+//tax4 =  * 0.4;
+var diff = 0;
+if(0 > salary <= 2500 ){
+    alert(tax)
+}
+else if(2500 < salary >= 50000){
+salary -= 25000;
+tax = salary * 0.2;//x
+alert(`You need to pay ${tax + tax1} tax`)
+else if(2500 < salary >= 50000){ 
+   diff = (salary - 25000) * 0.2;
+//salary -= 25000;
+//tax = salary * 0.2;//x
+alert(`You need to pay ${diff + tax1} tax`)
+}
+else if(50000 < salary >= 1000000){
+    salary -= 50000;
+    tax = salary * 0.3;
+    alert(`You need to pay ${tax + tax1 + tax2} tax`)
+}
+
+//diff = salary - 25000;
+*/
+
+//second try
+
+/*var salary = +prompt("enter your salary:");
 var tax = salary * 0.1;
 var tax1 = 25000 * 0.1;
 var tax2 = 25000 * 0.2;
@@ -10,8 +46,8 @@ if(0 > salary <= 2500 ){
 }
 else if(2500 < salary >= 50000){ 
    diff = (salary - 25000) * 0.2;
-/*salary -= 25000;
-tax = salary * 0.2;//x*/
+//salary -= 25000;
+//tax = salary * 0.2;//x
 alert(`You need to pay ${diff + tax1} tax`)
 }
 else if(50000 < salary >= 1000000){
@@ -21,4 +57,34 @@ else if(50000 < salary >= 1000000){
 }
 
 
-//diff = salary - 25000;
+//diff = salary - 25000;*/
+
+
+
+//third try
+const income = prompt("Enter your salary");
+function calculateTax(income){
+    let tax = 0;
+    let remainingIncome = 0;
+
+    switch(true){
+        case (income <= 25000):
+            tax = income * 0.1;
+            break;
+        case (income <= 50000):
+            tax = 25000 * 0.1 + (income - 25000) * 0.2;
+            break;
+        case (income <= 100000):
+            tax = 25000 * 0.1 + 25000 * 0.2 + (income - 50000) * 0.3;
+            break;
+        case (income <= 150000):
+            tax = 25000 * 0.1 + 25000 * 0.2 + 50000 * 0.3 + (income - 100000) * 0.4;
+            break;
+        default:
+            remainingIncome = income - 150000;
+            tax = 25000 * 0.1 + 25000 * 0.2 + 50000 * 0.3 + 50000 * 0.4 + remainingIncome * 0.5;
+            break;
+    }
+    return tax;
+}
+alert(calculateTax(income));
